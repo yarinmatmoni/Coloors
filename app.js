46466cda9409e536a0bc391df61ff07c6feba2b3
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-12-23 00:30:17 +0200

    Generate first color

---
+++ app.js
@@ -0,0 +1,21 @@
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+
+
+function generateHex() {
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors(){
+    colorDivs.forEach((div,index) =>{
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+
+randomColors();
