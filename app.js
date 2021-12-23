a571e27066623a37568eedea6c1c936b5feca6e0
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-12-23 16:20:59 +0200

    Checking Contrast

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ function generateHex() {
     return hexColor;
 }
 
-function randomColors(){
-    colorDivs.forEach((div,index) =>{
+function randomColors() {
+    colorDivs.forEach((div, index) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
+        checkTextContrast(randomColor,hexText);
     });
+}
+
+function checkTextContrast(color,text){
+    const luminance = chroma(color).luminance();
+    (luminance > 0.5) ? text.style.color = "black" : text.style.color = "white";
 }
 
 randomColors();
