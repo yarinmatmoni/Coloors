59be9dbdcdab1db9672f8ee3a06f39c5edecbc67
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-12-31 02:15:57 +0200

    open and close adjustments

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-continer");
+const adjustButton = document.querySelectorAll(".adjust");
+const closeAdjustments = document.querySelectorAll(".close-adjusment"); 
+const sliderContainers = document.querySelectorAll(".sliders");
 let initialColor;
 
 sliders.forEach((slider) => {
@@ -25,6 +28,18 @@ popup.addEventListener("transitionend", () => {
     const popupBox = popup.children[0];
     popup.classList.remove("active");
     popupBox.classList.remove("active");
+});
+
+adjustButton.forEach((button,index) => {
+    button.addEventListener("click", () => {
+        openAdjustmentPanel(index);
+    });
+});
+
+closeAdjustments.forEach((button,index) => {
+    button.addEventListener("click", ()=> {
+        closeAdjustmentPanel(index);
+    });
 });
 
 function generateHex() {
@@ -132,6 +147,14 @@ function copyToClipboard(hex){
     const popupBox = popup.children[0];
     popup.classList.add("active");
     popupBox.classList.add("active");
+}
+
+function openAdjustmentPanel(index){
+    sliderContainers[index].classList.toggle("active");
+}
+
+function closeAdjustmentPanel(index){
+    sliderContainers[index].classList.remove("active");
 }
 
 randomColors();
